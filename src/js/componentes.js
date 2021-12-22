53e984a1050bae7ad51e2136e9490c0ce809accd
Antonio DroCode <antuansabe@gmail.com>
2021-12-21 23:09:17 -0600

    todo app vanilla initial setup

---
+++ src/js/componentes.js
@@ -0,0 +1,15 @@
+import '../css/componentes.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
+
+
+export const saludar = ( nombre = 'sin nombre' ) => {
+    console.log('Creando etiqueta h1');
+
+    document.body.append( h1 );
+
+    
+    // Img
+    // const img = document.createElement('img');
+    // img.src = webpacklogo;
+    // document.body.append( img );
+}
