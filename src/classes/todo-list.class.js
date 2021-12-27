6be664b4cfba2cde6d41c1dcc942f9cc16a313c2
Antonio DroCode <antuansabe@gmail.com>
2021-12-26 22:45:42 -0600

    fix typo in todo-list-class file

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -2,7 +2,7 @@
 
 export class TodoList {
 
-    costructor() {
+    constructor() {
 
         this.todos = [];
 
--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import { Todo, TodoList } from './classes';
 const todoList = new TodoList();
 
 const tarea = new Todo('Aprender Javascript');
+console.log( tarea );
 
+todoList.nuevoTodo();
+
+console.log( todoList );
