0d655794c720e26ebe5cb250888c5d6f2b77740d
Antonio DroCode <antuansabe@gmail.com>
2021-12-31 14:29:11 -0600

    add anchorFiltros

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,7 +6,8 @@ import {  todoList } from '../index';
 const divTodoList = document.querySelector('.todo-list');
 const  txtInput   =  document.querySelector('.new-todo');
 const  btnBorrar  =  document.querySelector('.clear-completed');
-
+const  ulFiltros  =  document.querySelector('.filters');
+const anchorFiltro=  document.querySelectorAll('.filtro');
 
 export const crearTodoHtml = ( todo ) => {
 
@@ -77,4 +78,33 @@ btnBorrar.addEventListener('click', (event) => {
              }
 });
 
+ulFiltros.addEventListener('click', (event) => {
+
+   const filtro = event.target.text;
+   if( !filtro ) { return ;}
+
+   anchorFiltro.forEach( elem => elem.classList.remove('selected') );
+   event.target.classList.add('selected');
+
+
+   for( const elemento of divTodoList.children  ) {
+      elemento.classList.remove('hidden');
+      const completado = elemento.classList.contains('completed');
+
+      switch ( filtro ) {
+          case 'Pendientes':
+            if( completado ) {
+                elemento.classList.add('hidden');
+            }
+            break;
+          case 'Completados':
+              if( !completado ) {
+                  elemento.classList.add('hidden');
+              }
+              break;
+      }
+
+   };
+
+});
 
